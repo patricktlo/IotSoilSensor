50dc933091dd598eb913df1156861f2049254e4f
patrick <omairipatrick@gmail.com>
2018-09-23 23:21:24 -0300

    Got LaTeX doc working according to ABNT. Uploaded simulation files of the moisture sensor capacitor. Small changes to the board

---
+++ 4_Docs/Relatorio/auto/capitulo1.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "capitulo1"
+ (lambda ()
+   (LaTeX-add-labels
+    "chapter:intro"))
+ :latex)
+
---
+++ 4_Docs/Relatorio/auto/capitulo2.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "capitulo2"
+ (lambda ()
+   (LaTeX-add-labels
+    "chapter:fund"))
+ :latex)
+
